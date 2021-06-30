bc8a2c2fb3004dead0008049b0efc072c1e0c1ba
Johnny Barragan <johnhbarragan@gmail.com>
2021-06-29 22:40:58 -0700

    added for and while loops

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 'use strict'
 
-let answer = prompt('What is your name');
-
-
 let time = new Date().getHours(); //15
-let timeofday = '';
-let greeting = '';
+
+function getname(){
+let answer = prompt('What is your name?');
+return answer;
+
+}
+
+function hostCITY() {
+    let OlympicCity = prompt('Where is the next Olympics');
+    while (OlympicCity != 'Tokyo'){OlympicCity = prompt('Try Again, Where is next Olympics')};
+}
+
+function GoldCount() {
+    let medals = prompt('How many gold medals do you want?');
+for (let count = 0; count < medals; count++){
+    document.write ('<img src = "goldmedal.jpeg"/>');
+}
+}
+
+function morningafternoonevening(){
+    let timeofday = '';
 if (time < 12){
     timeofday = 'Good morning, ';
 } else if (time <17){
@@ -13,16 +29,35 @@ if (time < 12){
 } else{
     timeofday = 'Good evening, ';
 }
+return timeofday;
+}
+
+function amOrpm(time){
 if(time > 12){
     time = time - 12 + 'pm';
 } else {
     time = time + 'am';
 } 
+return time;
+}
 
+
+function greeting(timeofday, answer, time){
 if(answer === 'johnny'){
     greeting = timeofday + ' ' + answer + '. I hope you have a great day. The hour time is ' + time;
 } else {
     greeting = timeofday + 'today is a good day. The time right now is ' + time;
 }
-document.write (greeting); 
+return greeting;
+}
+
+let answer = getname();
+let OlympicCity = hostCITY();
+let medals = GoldCount()
+let timeofday = morningafternoonevening();
+let greetings = greeting(timeofday, answer, time);
+
+document.write (greetings); 
+
+
 
